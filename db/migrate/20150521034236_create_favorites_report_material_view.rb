090c9117ee4f7b59ec0528e8ee64d5bb5b2e9f32
Randy Burgess <wrburgess@gmail.com>
2015-05-28 23:17:47 -0500

    trying to fix the problematic mat view

--- db/migrate/20150521034236_create_favorites_report_material_view.rb
+++ db/migrate/20150521034236_create_favorites_report_material_view.rb
@@ -1,23 +1,23 @@
 class CreateFavoritesReportMaterialView < ActiveRecord::Migration
-  def up
-    execute <<-SQL
-    DROP MATERIALIZED VIEW IF EXISTS favorites_report;
-    CREATE MATERIALIZED VIEW favorites_report AS
-    SELECT
-      u.first_name As first_name,
-      u.last_name As last_name,
-      u.email As email,
-      u.roles[1] As role,
-      e.name As event_name
-    FROM favorites fa
-    INNER JOIN users u ON u.id = fa.user_id
-    INNER JOIN events e ON e.id = fa.event_id
-    SQL
-  end
+  # def up
+  #   execute <<-SQL
+  #   DROP MATERIALIZED VIEW IF EXISTS favorites_report;
+  #   CREATE MATERIALIZED VIEW favorites_report AS
+  #   SELECT
+  #     u.first_name As first_name,
+  #     u.last_name As last_name,
+  #     u.email As email,
+  #     u.roles[1] As role,
+  #     e.name As event_name
+  #   FROM favorites fa
+  #   INNER JOIN users u ON u.id = fa.user_id
+  #   INNER JOIN events e ON e.id = fa.event_id
+  #   SQL
+  # end
 
-  def down
-    execute <<-SQL
-      DROP MATERIALIZED VIEW IF EXISTS favorites_report
-    SQL
-  end
+  # def down
+  #   execute <<-SQL
+  #     DROP MATERIALIZED VIEW IF EXISTS favorites_report
+  #   SQL
+  # end
 end
