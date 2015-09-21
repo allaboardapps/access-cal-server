86c1bba0bee6cbb3cbd3fc912ebb957b8a0ea483
Randy Burgess <wrburgess@gmail.com>
2015-09-20 23:13:31 -0500

    updating gems

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -32,7 +32,7 @@ ActiveAdmin.register_page "Dashboard" do
     if Rails.env.development?
       panel "Mailer Testers" do
         ul do
-          li link_to "Test Form Submission Email", staff_view_form_submission_mailer_path
+          # li link_to "Test Form Submission Email", staff_view_form_submission_mailer_path
         end
       end
     end
