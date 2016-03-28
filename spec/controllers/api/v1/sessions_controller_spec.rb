c838820fdd84f1f07e160a611d4d9e55212e0650
Randy Burgess <wrburgess@gmail.com>
2016-03-27 19:10:22 -0500

    fixing the sessions controller

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -28,32 +28,32 @@ describe Api::V1::SessionsController, type: :controller do
       it "returns a user token that already exists on model" do
         user.update_attribute(:token, "1234")
         post :create, email: user.email, password: user.password
-        expect_json("user", { token: "1234" })
+        expect_json("data", attributes: { token: "1234" })
       end
 
       it "creates a user token if not present previously" do
         user.update_attribute(:token, nil)
         post :create, email: user.email, password: user.password
-        expect_json("user", { token: user.reload.token })
+        expect_json("data", attributes: { token: user.reload.token })
       end
 
       it "validates json attribute types" do
         user.update_attribute(:token, nil)
         post :create, email: user.email, password: user.password
-        expect_json_types("user", { token: :string })
-        expect_json_types("user", { id: :string })
-        expect_json_types("user", { first_name: :string })
-        expect_json_types("user", { last_name: :string })
-        expect_json_types("user", { email: :string })
-        expect_json_types("user", { zip_code: :string })
-        expect_json_types("user", { time_zone: :string })
-        expect_json_types("user", { location_id: :string })
-        expect_json_types("user", { roles: :array })
-        expect_json_types("user", { statuses: :array })
-        expect_json_types("user", { archived: :boolean })
-        expect_json_types("user", { test: :boolean })
-        expect_json_types("user", { updated_at: :date })
-        expect_json_types("user", { created_at: :date })
+        expect_json_types("data", attributes: { token: :string })
+        expect_json_types("data", { id: :string })
+        expect_json_types("data", attributes: { first_name: :string })
+        expect_json_types("data", attributes: { last_name: :string })
+        expect_json_types("data", attributes: { email: :string })
+        expect_json_types("data", attributes: { zip_code: :string })
+        expect_json_types("data", attributes: { time_zone: :string })
+        expect_json_types("data", attributes: { location_id: :string })
+        expect_json_types("data", attributes: { roles: :array })
+        expect_json_types("data", attributes: { statuses: :array })
+        expect_json_types("data", attributes: { archived: :boolean })
+        expect_json_types("data", attributes: { test: :boolean })
+        expect_json_types("data", attributes: { updated_at: :date })
+        expect_json_types("data", attributes: { created_at: :date })
       end
 
       it "returns a status of 200" do
