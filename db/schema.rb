b20ac80e421cd610879b2a6d798f43781cd534cb
Randy Burgess <wrburgess@gmail.com>
2016-04-04 13:41:04 -0500

    adding tag category attr

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 20160327201109) do
   end
 
   create_table "event_tags", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
-    t.string   "event_id"
-    t.string   "tag_id"
+    t.uuid     "event_id"
+    t.uuid     "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -153,8 +153,9 @@ ActiveRecord::Schema.define(version: 20160327201109) do
     t.string   "name"
     t.string   "description"
     t.string   "tag_type"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "tag_category"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name { Faker::Superhero.name }
     description { Faker::Hipster.sentence(8) }
     tag_type { TagTypes.all.sample }
+    tag_category { TagCategories.all.sample }
   end
 end
