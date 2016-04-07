4f7e55e657a5e772b050986017778635258537ad
Randy Burgess <wrburgess@gmail.com>
2016-04-07 00:04:44 -0500

    fixins

--- config/application.rb
+++ config/application.rb
@@ -26,20 +26,24 @@ module AccessCalServer
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.insert_before 0, "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins "*"
 
-        resource "/cors",
+        resource(
+          "/cors",
           headers: :any,
           methods: [:post],
           credentials: true,
           max_age: 0
+        )
 
-        resource "*",
+        resource(
+          "*",
           headers: :any,
           methods: [:get, :post, :delete, :put, :options, :head],
           max_age: 0
+        )
       end
     end
   end
