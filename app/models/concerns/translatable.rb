99d822303aed4eb8d0508da064f771ca8694df47
Randy Burgess <wrburgess@gmail.com>
2016-05-11 00:07:05 -0500

    adding retrieve method

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -2,11 +2,19 @@ module Translatable
   extend ActiveSupport::Concern
 
   def self.translated(*args)
-    args.map{ |a| a.to_sym }
+    args.map(&:to_sym)
+  end
+
+  def retrieve(attribute, key)
+    raise(ArgumentError, "Attribute must be of type :jsonb. Attribute `#{attribute}` is of type #{column_for_attribute(attribute).type}.") unless column_for_attribute(attribute).type == :jsonb
+
+    str ||= send(attribute.to_s)[key.to_s]
+    return send(attribute.to_s)["en"] if str.nil?
+    str
   end
 
   def insert(attribute, key = :en, str = "")
-    raise ArgumentError.new("Attribute must be of type :jsonb. Attribute `#{attribute.to_s}` is of type #{column_for_attribute(attribute).type}.") unless column_for_attribute(attribute).type == :jsonb
+    raise(ArgumentError, "Attribute must be of type :jsonb. Attribute `#{attribute}` is of type #{column_for_attribute(attribute).type}.") unless column_for_attribute(attribute).type == :jsonb
 
     send(attribute.to_s)[key.to_s] = str
     save
@@ -15,6 +23,6 @@ module Translatable
   def uniq_keys(attribute)
     return "nope" unless column_for_attribute(attribute).type == :jsonb
 
-    send(attribute.to_s).map{ |k,v| k.to_sym }.flatten
+    send(attribute.to_s).map { |k, _v| k.to_sym }.flatten
   end
 end
